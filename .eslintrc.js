18583081073856b6ad19919de46361c1c6670515
Nhat Tran <nhat.tran416@icloud.com>
2025-05-01 16:33:15 -0400

    chore(ci): add lint step and ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist/', 'node_modules/'],
+  rules: {}
+};
